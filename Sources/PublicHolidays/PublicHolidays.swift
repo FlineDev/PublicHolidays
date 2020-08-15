419e460d497886ac2ce6fc4466b79dec89bdbd88
Cihat Gündüz <commits@cihatguenduez.de>
2020-08-15 18:30:22 +0200

    Add function to check if date is on public holiday

--- Sources/PublicHolidays/PublicHolidays.swift
+++ Sources/PublicHolidays/PublicHolidays.swift
@@ -1,19 +1,17 @@
 import Foundation
+import HandySwift
 
 enum PublicHolidaysError: Error {
     case unavailableCountryCode(String)
     case unavailableSubTerritoryCode(String)
+    case dateOutsideOfSupportedRange
 }
 
 /// The main API to use for fetching public holiday.
 public enum PublicHolidays {
-    private static var jsonDataDirUrl = URL(fileURLWithPath: Bundle.module.resourcePath!).appendingPathComponent("JsonData")
+    public static let supportedYears: ClosedRange<Int> = 2020...2029
 
-    private static var jsonDecoder: JSONDecoder = {
-        let jsonDecoder = JSONDecoder()
-        jsonDecoder.dateDecodingStrategy = .formatted(DateFormatter.dateOnly)
-        return jsonDecoder
-    }()
+    private static var jsonDataDirUrl = URL(fileURLWithPath: Bundle.module.resourcePath!).appendingPathComponent("JsonData")
 
     /// Returns the ISO codes of all countries that have available public holildays data.
     /// - NOTE:Even if this responds with a non-empty list of sub territories, the list is not guaranteed to be complete.
@@ -25,12 +23,13 @@ public enum PublicHolidays {
 
     /// Returns a list of ISO sub territory codes (2/3 numbers/characters) for the given country code.
     public static func availableSubTerritories(countryCode: String) throws -> [String] {
-        try loadCountry(countryCode: countryCode).subTerritories.map { $0.isoCode }
+        try loadCountry(countryCode: countryCode, timeZone: Calendar.current.timeZone).subTerritories.map { $0.isoCode }
     }
 
     /// Returns a sorted list of all public holidays for the given country or sub territory (optional).
-    public static func all(countryCode: String, subTerritoryCode: String? = nil) throws -> [PublicHoliday] {
-        let country = try loadCountry(countryCode: countryCode)
+    /// - NOTE: Always uses current calendars time zone.
+    public static func all(countryCode: String, subTerritoryCode: String? = nil, timeZone: TimeZone = Calendar.current.timeZone) throws -> [PublicHoliday] {
+        let country = try loadCountry(countryCode: countryCode, timeZone: timeZone)
 
         if let subTerritoryCode = subTerritoryCode {
             guard let subTerritory = country.subTerritories.first(where: { $0.isoCode == subTerritoryCode }) else {
@@ -43,11 +42,34 @@ public enum PublicHolidays {
         }
     }
 
-    private static func loadCountry(countryCode: String) throws -> Country {
+    /// Returns `true` if the given date is on a public holiday for the given country (and sub territory).
+    /// - NOTE: Always uses current calendars time zone. u
+    public static func contains(
+        date: Date,
+        countryCode: String,
+        subTerritoryCode: String? = nil,
+        timeZone: TimeZone = Calendar.current.timeZone
+    ) throws -> Bool {
+        guard supportedYears.contains(date.year(timeZone: timeZone)) else { throw PublicHolidaysError.dateOutsideOfSupportedRange }
+        let publicHolidaysAtLocation = try all(countryCode: countryCode, subTerritoryCode: subTerritoryCode, timeZone: timeZone)
+
+        guard let firstPublicHolidayEndingAfterDate = publicHolidaysAtLocation.first(where: { $0.date.addingTimeInterval(.days(1)) > date }) else {
+            return false
+        }
+        return firstPublicHolidayEndingAfterDate.date <= date
+    }
+
+    private static func loadCountry(countryCode: String, timeZone: TimeZone) throws -> Country {
         guard let countryData = FileManager.default.contents(atPath: jsonDataDirUrl.appendingPathComponent("\(countryCode).json").path) else {
             throw PublicHolidaysError.unavailableCountryCode(countryCode)
         }
 
-        return try jsonDecoder.decode(Country.self, from: countryData)
+        return try jsonDecoder(timeZone: timeZone).decode(Country.self, from: countryData)
+    }
+
+    private static func jsonDecoder(timeZone: TimeZone) -> JSONDecoder {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.dateDecodingStrategy = .formatted(DateFormatter.dateOnly(timeZone: timeZone))
+        return jsonDecoder
     }
 }
--- Tests/PublicHolidaysTests/Support/XCTestCaseExt.swift
+++ Tests/PublicHolidaysTests/Support/XCTestCaseExt.swift
@@ -1,6 +1,6 @@
 import Difference
 import XCTest
 
-func XCTAssertEqual<T: Equatable>(_ expected: T, _ received: T, file: StaticString = #filePath, line: UInt = #line) {
+func XCTAssertEqual<T: Equatable>(_ received: T, _ expected: T, file: StaticString = #filePath, line: UInt = #line) {
     XCTAssertTrue(expected == received, "Found difference for \n" + diff(expected, received).joined(separator: ", "), file: file, line: line)
 }
